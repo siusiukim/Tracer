fc4cfa16727dc401a22538c56d24b8b4f167583b
siusiukim <siukim1611@gmail.com>
2019-04-03 11:45:00 +0800

    shadowAttenuation

--- src/scene/light.cpp
+++ src/scene/light.cpp
@@ -13,7 +13,19 @@ vec3f DirectionalLight::shadowAttenuation( const vec3f& P ) const
 {
     // YOUR CODE HERE:
     // You should implement shadow-handling code here.
-    return vec3f(1,1,1);
+
+	vec3f direction = getDirection(P);
+	vec3f currentPoint = P;
+	ray shadowRay(currentPoint, direction);
+	vec3f coleur = getColor(P);
+	isect intersectPt;
+	while ((scene->intersect(shadowRay, intersectPt)) == true) {
+		if (intersectPt.getMaterial().kt.iszero()) return vec3f(0, 0, 0); //Soleil source is completely blocked
+		currentPoint = shadowRay.at(intersectPt.t);
+		shadowRay = ray(currentPoint, direction);
+		coleur = prod(coleur, intersectPt.getMaterial().kt);
+	}
+	return coleur;
 }
 
 vec3f DirectionalLight::getColor( const vec3f& P ) const
@@ -34,6 +46,10 @@ double PointLight::distanceAttenuation( const vec3f& P ) const
 	// You'll need to modify this method to attenuate the intensity 
 	// of the light based on the distance between the source and the 
 	// point P.  For now, I assume no attenuation and just return 1.0
+
+	double d2 = (P - position).length_squared();
+	double d = sqrt(d2);
+	
 	return 1.0;
 }
 
@@ -53,5 +69,20 @@ vec3f PointLight::shadowAttenuation(const vec3f& P) const
 {
     // YOUR CODE HERE:
     // You should implement shadow-handling code here.
-    return vec3f(1,1,1);
+
+	const vec3f &dir = getDirection(P);
+	isect i;
+	ray R(P, dir);
+	if (scene->intersect(R, i)) {
+		const double light_t = (position - P).length();
+		if (i.t < light_t) {
+			return vec3f();
+		}
+		else {
+			return vec3f(1.0, 1.0, 1.0);
+		}
+	}
+	else {
+		return vec3f(1.0, 1.0, 1.0);
+	}
 }
